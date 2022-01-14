636f2cb5693fde69524897cc66d2af7d54c16e77
Eoin Lynch <c16310846@mydit.ie>
2022-01-14 21:44:24 +0000

    Add files via upload

---
+++ Year 2/Python/work_2018_2019/week2/week2.py
@@ -0,0 +1,8 @@
+#Week 2 Lab Exercise
+#Converting an int into a binary string and then back to int
+
+my_str = input("Enter an integer: ")
+my_int = int(my_str)
+
+"{0:b}".format(my_int)
+print(my_str)
