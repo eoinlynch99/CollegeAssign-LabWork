14a05dd2b355cdad6182d1eb57ee2a6f1b7a1813
Eoin Lynch <c16310846@mydit.ie>
2022-01-14 21:30:34 +0000

    Add files via upload

---
+++ Year 1/Lab1/src/Vehicle.java
@@ -0,0 +1,14 @@
+/****************************************
+*    Control: Lab to create a java class
+*   Author: Eoin Lynch
+*   Date:05/03/2018
+*****************************************/
+
+public class Vehicle 
+{
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Year 1/Lab1/src/lab1part1.java
@@ -0,0 +1,15 @@
+/****************************************
+*    Control: Lab to create a java class
+*   Author: Eoin Lynch
+*   Date:05/03/2018
+*****************************************/
+
+public class lab1part1 
+{	
+	public static void main(String[] args) {
+		
+		System.out.print("Hello World");
+
+	}
+
+}
