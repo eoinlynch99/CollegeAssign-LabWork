f10b4ef3c1b5c8faa2bfed1db07973438ddbfe62
Eoin Lynch <c16310846@mydit.ie>
2022-01-14 21:48:14 +0000

    Delete Web_Dev_Project directory

--- Web_Dev_Project/darceair/UnsetAll.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION["Selected"]);
-	unset($_SESSION["DateSelected"]);
-	unset($_SESSION["currentFile"]);
-	session_destroy();
-	
-	header("Location:flight_times.php");
-?>
